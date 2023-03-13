0d6c7355a4a95efa41566c8dd88353c6d10065c5
cristianmflorez <cristianmflorez@gmail.com>
2023-03-13 11:51:20 -0500

    vercel 3

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -15,7 +15,7 @@ function Profile(params) {
 
     
     useEffect(()=> {
-        fetch(`https://web-production-009b.up.railway.app/profile/${id}`,{method: 'GET', credentials:'include'})
+        fetch(`https://backend-allsales.vercel.app/profile/${id}`,{method: 'GET', credentials:'include'})
             .then(response => response.json())
             .then(data => {
                 if(data.statusCode===404){
